79bd35552b904326862a86047f45f1cbffa6aef7
SymenYang <SymenYang@163.com>
2021-12-30 16:12:23 +0800

    Change "model" to "runner" in analyze scripts

--- DLNest/FactoryFiles/FactoryClean/AnalyzeScripts/test.py
+++ DLNest/FactoryFiles/FactoryClean/AnalyzeScripts/test.py
@@ -1,2 +1,2 @@
 def experience(self):
-    print(self.model,self.dataset,self.args)
+    print(self.runner,self.dataset,self.args)
--- DLNest/FactoryFiles/FactoryMNIST/AnalyzeScripts/getAcc.py
+++ DLNest/FactoryFiles/FactoryMNIST/AnalyzeScripts/getAcc.py
@@ -6,10 +6,10 @@ def experience(self):
     total = 0
     for _iter,data in enumerate(valLoader):
         x,y = data
-        if self.model._envType != "CPU":
+        if self.runner._envType != "CPU":
             x,y = x.cuda(),y.cuda()
         with torch.no_grad():
-            output = self.model.model(x)
+            output = self.runner.model(x)
             _,pred = torch.max(output,1)
             acc += sum(pred == y).item()
             total += x.shape[0]
